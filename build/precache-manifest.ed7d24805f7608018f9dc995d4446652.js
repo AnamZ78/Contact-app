de8d5132e0b3c4158937493caf5f132701752ee3
Anam <anamzahid126@gmail.com>
2023-03-27 12:21:54 +0530

    first commit

---
+++ build/precache-manifest.ed7d24805f7608018f9dc995d4446652.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ced132167ff6796b1e5eecccda53d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aad9d73243c79cd31361",
+    "url": "/static/js/2.6e1939c1.chunk.js"
+  },
+  {
+    "revision": "ef19423f500b77600728",
+    "url": "/static/js/main.d5aa7738.chunk.js"
+  },
+  {
+    "revision": "e28598308d05696b44ef",
+    "url": "/static/js/runtime-main.c778fba5.js"
+  }
+]);
